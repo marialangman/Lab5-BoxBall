5fa32c2e2fe30a007ff348c7250d171654da5b1e
marialangman <g00227867@stu.raritanval.edu>
2015-10-26 11:06:53 -0400

    Maria Langman Bouncing Balls Project

--- BallDemo.java
+++ BallDemo.java
@@ -1,23 +1,26 @@
 import java.awt.Color;
 
 /**
- * Class BallDemo - a short demonstration showing animation with the 
- * Canvas class. 
- *
- * @author Michael Kölling and David J. Barnes
+ * Class BallDemo - a short demonstration showing animation with the Canvas class.  
+ * Added boxBounce method to have balls bounce inside a box within Canvas.
+ * 
+ * @author Michael Kölling and David J. Barnes / Edited by Maria Langman. October 2015
  * @version 2011.07.31
- */
+ *
+ * */
 
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private final int WIDTH = 600;
+    private final int HEIGHT = 500;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
-        myCanvas = new Canvas("Ball Demo", 600, 500);
+        myCanvas = new Canvas("Ball Demo", WIDTH, HEIGHT);
     }
 
     /**
@@ -25,14 +28,13 @@ public class BallDemo
      */
     public void bounce()
     {
+       // myCanvas.erase();   //refresh the Canvas
         int ground = 400;   // position of the ground line
-
-        myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
+        // create and show the balls
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
         BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
@@ -49,5 +51,54 @@ public class BallDemo
                 finished = true;
             }
         }
+    }
+
+    /**
+     * Method boxBounce is responsible for drawing four boundaries within the canvas such that BoxBall objects 
+     * can bounce only inside the boundaries, which are determined by a final int named OFFSET that is drawn 
+     * from the edges of the canvas.  Therefore, the WIDTH of the canvas and the HEIGHT set the 
+     * boundaries of the lines as per OFFSET.  Bouncing will end as controlled by a 'timer'.
+     * @param numberOfBalls to specify how many balls are in the box.
+     * @param offset value to set the boundaries of the box
+     */
+    public void boxBounce(int numberOfBalls, int offset)
+    {
+        myCanvas.erase();   //refresh the Canvas
+        myCanvas.setVisible(true);
+        final int OFFSET = offset; //offset from the boundaries
+        final int X_MAX = WIDTH - OFFSET;
+        final int Y_MAX = HEIGHT - OFFSET;
+
+        //Create BoxBall objects array
+        BoxBall[] balls = new BoxBall[numberOfBalls];
+
+        //Create the array of balls
+        for (int i=0; i<balls.length; i++) 
+        {
+            balls[i] = new BoxBall(X_MAX, Y_MAX, myCanvas, OFFSET);
+        }
+
+        //Bounce the balls
+        int timer=0;       
+        while (timer<200)
+        {
+            myCanvas.wait(30);           // small delay
+            
+            //draw boundaries each simulation "tick" to 'mask' erase() method's peculiarity
+            //when object is at any of the boundaries
+            myCanvas.drawLine(OFFSET, OFFSET, X_MAX, OFFSET);   //top boundary
+            myCanvas.drawLine(X_MAX, OFFSET, X_MAX, Y_MAX);     //right boundary
+            myCanvas.drawLine(OFFSET, Y_MAX, X_MAX, Y_MAX);     //bottom boundary
+            myCanvas.drawLine(OFFSET, OFFSET, OFFSET, Y_MAX);   //left boundary
+
+            //bounce each  balls
+            for (int i=0; i<balls.length; i++)
+            {
+                balls[i].move();
+            }
+
+            timer++;
+        }
+
     }
 }
---
+++ BoxBall.java
@@ -0,0 +1,129 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.Random;
+/**
+ * Class to create BoxBall objects that have random speed, random diameter, random color, and random intial 
+ * coordinates.  They bounce off set boundaries, determined by BallDemo Canvas object by an offset value.
+ * 
+ * @author Maria Langman 
+ * @version October 26, 2105
+ */
+public class BoxBall
+{
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;
+    private int ySpeed;                 // initial downward speed
+    private int xSpeed;                // initial horizontal speed
+    private Canvas canvas;
+    private int x_Max;
+    private int y_Max;
+    private int minValue;               //due to offset for boundaries
+    /**
+     * Constructor for objects of class BoxBall
+     * Instance variables color, diameter, xPosition, yPosition, xSpeed, and ySpeed generated randomly while
+     * minValue holds the offset value from the edge of the canvas.
+     * @param xMax  the maximum value for the x-coordinate so it bounces within the given bounds
+     * @param yMax  the maximum value for the y-coordinate so it bounces within the given bounds
+     * @param drawingCanvas  the canvas to draw this ball on
+     * @param offset the value (offset) from the edge of the canvas
+     */
+    public BoxBall(int xMax, int yMax, Canvas drawingCanvas, int offset)
+    {
+        Random generator = new Random();
+
+        //generate random diameter;
+        diameter = generator.nextInt(20)+5;  //at least 5px so the ball can be seen
+
+        //generate random speed in x and y directions
+        xSpeed = generator.nextInt(7)+1;    //ensure no horizontal
+        ySpeed = generator.nextInt(7)+1;    //ensure no vertical
+
+        //genereate a random rgb for color
+        int r = generator.nextInt(250);
+        int g = generator.nextInt(250);
+        int b = generator.nextInt(250);
+        color = new Color(r, g, b);
+
+        canvas = drawingCanvas;
+
+        x_Max = xMax;
+        y_Max = yMax;
+        minValue = offset;
+
+        //generate random coordinates of BoxBall object
+        xPosition = generator.nextInt(xMax);
+        yPosition = generator.nextInt(yMax);
+
+    }
+
+    /**
+     * Draw this ball at its current position onto the canvas.
+     **/
+    public void draw()
+    {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }    
+
+    /**
+     * Move this ball so its directional speed changes (deflects) when it hits a boundary.  Set position
+     * and speed then redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+        erase();
+
+        //move the ball at set speed
+        xPosition+=xSpeed;
+        yPosition+=ySpeed; 
+
+        // check if it has hit a boundary
+        if (xPosition < minValue) {
+            xPosition = diameter + minValue;
+            xSpeed = -xSpeed;
+        }
+        if (xPosition > x_Max - diameter) {
+            xPosition = x_Max - diameter;
+            xSpeed = -xSpeed;
+        }
+        if (yPosition < minValue) {
+            yPosition = diameter + minValue;
+            ySpeed = -ySpeed;
+        }
+        if (yPosition > y_Max - diameter) {
+            yPosition = y_Max - diameter;
+            ySpeed = -ySpeed;
+        }
+
+        // draw again at new position
+        draw();
+
+    }    
+
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
+    }
+}
